b3a2e3b889f6cf2fa05ff8de960855eed178660f
Emil <92583822+Proto69@users.noreply.github.com>
2023-12-03 08:01:16 +0000

    Created the models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type')->nullable();
+            $table->string('type');
             $table->string('email')->unique();
-            $table->string('profile_photo_path', 2048)->nullable();
-
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
