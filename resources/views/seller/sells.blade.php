0949e589a70578c3959665eb75253d14f6b98d2c
Tsvetoslav <98642746+CeCiToU@users.noreply.github.com>
2024-02-01 11:31:41 +0000

    log layout created

--- resources/views/seller/sells.blade.php
+++ resources/views/seller/sells.blade.php
@@ -7,8 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                
+            <div class="bg-white dark:bg-gray-800 overflow-hidden py-1 shadow-xl sm:rounded-lg mx-10 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-3">
+                <p class="text-center tracking-tighter text-gray-500 md:text-lg dark:text-gray-400"><strong>Smartphone</strong> x <strong>12</strong> броя. Продадено на <strong>2024.02.01</strong> за <strong>7200</strong>лв.</p>
             </div>
         </div>
     </div>
