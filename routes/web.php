fa4d2add379c2a50023d09d71170a6ca460a9629
Emil <92583822+Proto69@users.noreply.github.com>
2024-02-17 13:54:48 +0000

    working on design on welcome page

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/new-product', [PageController::class, 'newProduct'])->name('new-product');
     Route::get('/edit-product/{product_id}', [PageController::class, 'editProduct'])->name('edit-product');
     // Working on it
-    // Route::get('/stats', [PageController::class, 'stats'])->name('stats');
+    Route::get('/stats', [PageController::class, 'stats'])->name('stats');
+    //
     Route::get('/connect-stripe', [PageController::class, 'connectStripe'])->name('connect-stripe');
     Route::get('/refresh-stripe', [PageController::class, 'refreshStripe'])->name('refresh-stripe');
     Route::get('/return-stripe', [PageController::class, 'returnStripe'])->name('return-stripe');
